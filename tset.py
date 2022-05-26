a308f1d73d9e2e73730e8271d38270c18e2c7860
sheakhu007 <methekingalways@gmail.com>
2022-05-26 14:59:02 +0530

    first commit'

---
+++ tset.py
@@ -0,0 +1,11 @@
+import psycopg2
+from psycopg2 import Error
+from sqlalchemy import create_engine, inspect
+import pandas as pd
+from sqlalchemy import create_engine
+df = pd.read_csv("testset.csv")
+engine = create_engine('postgresql://postgres:postgres@localhost:5432/postgres')
+df.to_sql("my_table", engine,if_exists='replace')
+data = engine.execute('SELECT * FROM my_table').fetchall()
+data = pd.DataFrame(data)
+print(data)
