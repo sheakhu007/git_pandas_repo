12465510aa50ac139d320afc2da7b55f00dddabf
sheakhu007 <methekingalways@gmail.com>
2022-05-31 00:46:13 +0530

    Check node name

--- tset.py
+++ tset.py
@@ -1,6 +1,4 @@
-import psycopg2
-from psycopg2 import Error
-from sqlalchemy import create_engine, inspect
+from sqlalchemy import create_engine
 import pandas as pd
 from sqlalchemy import create_engine
 df = pd.read_csv("testset.csv")
@@ -8,3 +6,4 @@ engine = create_engine('postgresql://postgres:postgres@localhost:5432/postgres')
 df.to_sql("my_table", engine,if_exists='replace')
 data = engine.execute('SELECT * FROM my_table').fetchall()
 data = pd.DataFrame(data)
+print("Commit C")
